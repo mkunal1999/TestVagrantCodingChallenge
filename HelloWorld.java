d1de5535d77da8967c92ccd097a03d13150d3684
mkunal1999 <mkunal1999@gmail.com>
2022-12-03 12:26:00 +0530

    Update
    Update

--- HelloWorld.java
+++ HelloWorld.java
@@ -57,7 +57,6 @@ class SolarSystem{
                     freq.put(s, count + 1);
                 }
             }
-            
         }
         return Collections.max(freq.entrySet(), (entry1, entry2) -> entry1.getValue() - entry2.getValue()).getKey();
     }
@@ -67,20 +66,24 @@ class HelloWorld {
     public static void main(String[] args) {
         List<String> surfaceGasses = new ArrayList<String>();
         Planet p1 = new Planet("Mercury", surfaceGasses, 0, false);
-        surfaceGasses.add("Carbon Dioxide");
-        surfaceGasses.add("Nitrogen");
-        Planet p2 = new Planet("Venus", surfaceGasses, 0, false);
         List<String> surfaceGasses2 = new ArrayList<String>();
+        surfaceGasses2.add("Carbon Dioxide");
         surfaceGasses2.add("Nitrogen");
-        surfaceGasses2.add("Oxygen");
-        Planet p3 = new Planet("Earth", surfaceGasses2, 1, false);
+        Planet p2 = new Planet("Venus", surfaceGasses2, 0, false);
         List<String> surfaceGasses3 = new ArrayList<String>();
-        surfaceGasses3.add("Hydrogen");
-        surfaceGasses3.add("Helium");
-        Planet p4 = new Planet("Jupitor", surfaceGasses3, 79, true);
-        Planet p5 = new Planet("Saturn", surfaceGasses3, 83, true);
-        surfaceGasses3.add("Methane");
-        Planet p6 = new Planet("Uranus", surfaceGasses3, 27, true);
+        surfaceGasses3.add("Nitrogen");
+        surfaceGasses3.add("Oxygen");
+        Planet p3 = new Planet("Earth", surfaceGasses3, 1, false);
+        List<String> surfaceGasses4 = new ArrayList<String>();
+        surfaceGasses4.add("Hydrogen");
+        surfaceGasses4.add("Helium");
+        Planet p4 = new Planet("Jupitor", surfaceGasses4, 79, true);
+        Planet p5 = new Planet("Saturn", surfaceGasses4, 83, true);
+        List<String> surfaceGasses5 = new ArrayList<String>();
+        surfaceGasses5.add("Hydrogen");
+        surfaceGasses5.add("Helium");
+        surfaceGasses5.add("Methane");
+        Planet p6 = new Planet("Uranus", surfaceGasses5, 27, true);
         
         List<Planet> planets = new ArrayList<Planet>();
         planets.add(p1);
